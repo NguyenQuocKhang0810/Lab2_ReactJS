ee94389c106eac895366a2de8ef50e54cbcf4ca7
Nguyen Quoc Khang <quockhang972@gmail.com>
2024-09-24 19:19:48 +0700

    Lab2 update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./Header";
+import MyCarousel from "./Carousel";
 
 const Banner = () => {
   return (
     <div className="hero_area">
       <Header></Header>
       <div className="bg-box">
-        <img src="/pizza1.jpg" alt="" />
-        <div className="overlay"></div>
+        {/* <img src="/pizza1.jpg" alt="" />
+        <div className="overlay"></div> */}
+        <MyCarousel></MyCarousel>
       </div>
     </div>
   );
